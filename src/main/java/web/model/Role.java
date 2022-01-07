2d2604825331c05411babf99ffddd5b3374acd6f
yainesk99 <yainesk1999@gmail.com>
2022-01-07 13:00:04 +0300

    Changes

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -22,39 +22,7 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
-    @Override
-    public String getAuthority() {
-        return name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role = (Role) o;
-        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
-    }
 
     @Override
     public int hashCode() {
